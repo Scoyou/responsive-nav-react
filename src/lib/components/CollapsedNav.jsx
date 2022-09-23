2e3213d104f2b307aa12295e0eb9e26084824e0d
Scott Young <scott.young@getbridge.com>
2022-09-23 14:25:25 -0600

    initial commit

---
+++ src/lib/components/CollapsedNav.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "../styles/index.css";
+
+const CollapsedNav = ({
+  setShowCollapsedNav,
+  menuIcon,
+  iconHeight,
+  iconWidth,
+}) => {
+  return (
+    <div className="collapsed-nav">
+      <img
+        alt="nav menu"
+        className="nav-item"
+        src={menuIcon}
+        onClick={() => setShowCollapsedNav(false)}
+        height={iconHeight}
+        width={iconWidth}
+      />
+      {...children}
+    </div>
+  );
+};
+
+export default CollapsedNav;
---
+++ src/lib/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import useWindowDimensions from "../hooks/useWindowDimensions";
+import "../styles/index.css";
+import CollapsedNav from "./CollapsedNav";
+
+const Navbar = ({ navTitle, menuIcon, iconHeight, iconWidth }) => {
+  const [colorChange, setColorchange] = useState(false);
+  const [showCollapsedNav, setShowCollapsedNav] = useState(false);
+  const { width } = useWindowDimensions();
+
+  const changeNavbarColor = () => {
+    if (window.scrollY >= 80) {
+      setColorchange(true);
+    } else {
+      setColorchange(false);
+    }
+  };
+  window.addEventListener("scroll", changeNavbarColor);
+
+  return (
+    <div className={colorChange ? "navbar colorChange" : "navbar"}>
+      {showCollapsedNav && (
+        <CollapsedNav setShowCollapsedNav={setShowCollapsedNav} />
+      )}
+      <span className="nav-title">{navTitle}</span>
+      {width < 600 ? (
+        <img
+          alt="nav menu"
+          className="nav-item"
+          src={menuIcon}
+          onClick={() => setShowCollapsedNav(true)}
+          height={iconHeight}
+          width={iconWidth}
+        />
+      ) : (
+        <>{...children}</>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
